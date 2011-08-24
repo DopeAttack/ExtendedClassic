53c01faaf8a7c8f18b95e262a960348ef9e7714b
DopeAttack <DopeAttack@extended-wow.de>
2011-08-24 03:51:45 +0200

    Fix Krieger Wut Formel

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -1855,7 +1855,7 @@ void Player::RewardRage( uint32 damage, uint32 weaponSpeedHitFactor, bool attack
 
     if(attacker)
     {
-        addRage = ((damage/rageconversion*7.5f + weaponSpeedHitFactor)/2.0f);
+        addRage = (damage/rageconversion)*7.5f;
     }
     else
     {
